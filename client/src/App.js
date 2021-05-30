050cd6c8c3a1292bb248e54cc6e8a203b2829491
Mirabror Sodikov <mirabror9545@gmail.com>
2021-05-30 19:37:55 +0500

    semantic-ui, routes, menubar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
+import MenuBar from './component/MenuBar'
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+
 function App() {
-    return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
-    )
+  return (
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path='/' component={HomeScreen} />
+        <Route exact path='/register' component={RegisterScreen} />
+        <Route exact path='/login' component={LoginScreen} />
+      </Container>
+    </Router>
+  )
 }
 
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
+import ApolloProvider from './ApolloProvider'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>{ApolloProvider}</React.StrictMode>,
+  document.getElementById('root')
 )
 
 // If you want to start measuring performance in your app, pass a function

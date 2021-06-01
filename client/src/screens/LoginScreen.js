2d54d2f1d0a3f82c6a529564a78d026df57ea8f1
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-01 23:08:18 +0500

    login, register page responsive

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react'
 import { useMutation, gql } from '@apollo/client'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Container } from 'semantic-ui-react'
 
 import { AuthContext } from '../context/auth'
 import { useForm } from '../utils/hooks'
@@ -30,41 +30,48 @@ export default function LoginScreen(props) {
   }
 
   return (
-    <div className='form-container'>
-      <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
-        <h1>Login</h1>
-        <Form.Input
-          label='Username'
-          placeholder='Enter username...'
-          type='text'
-          name='username'
-          value={values.username}
-          onChange={onChange}
-          error={errors.username ? true : false}
-        />
-        <Form.Input
-          label='Password'
-          placeholder='Enter password...'
-          type='password'
-          name='password'
-          value={values.password}
-          onChange={onChange}
-          error={errors.password ? true : false}
-        />
-        <Button type='submit' primary>
-          Login
-        </Button>
-      </Form>
-      {Object.keys(errors).length > 0 && (
-        <div className='ui error message'>
-          <ul className='list'>
-            {Object.values(errors).map((value) => (
-              <li key={value}>{value}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <Container>
+      <div className='form-container'>
+        <Form
+          onSubmit={onSubmit}
+          noValidate
+          className={loading ? 'loading' : ''}
+          mobile={16}
+        >
+          <h1>Login</h1>
+          <Form.Input
+            label='Username'
+            placeholder='Enter username...'
+            type='text'
+            name='username'
+            value={values.username}
+            onChange={onChange}
+            error={errors.username ? true : false}
+          />
+          <Form.Input
+            label='Password'
+            placeholder='Enter password...'
+            type='password'
+            name='password'
+            value={values.password}
+            onChange={onChange}
+            error={errors.password ? true : false}
+          />
+          <Button type='submit' primary>
+            Login
+          </Button>
+        </Form>
+        {Object.keys(errors).length > 0 && (
+          <div className='ui error message'>
+            <ul className='list'>
+              {Object.values(errors).map((value) => (
+                <li key={value}>{value}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </Container>
   )
 }
 
--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react'
 import { useMutation, gql } from '@apollo/client'
-import { Form, Button } from 'semantic-ui-react'
+import { Form, Button, Container } from 'semantic-ui-react'
 
 import { AuthContext } from '../context/auth'
 import { useForm } from '../utils/hooks'
@@ -32,59 +32,65 @@ export default function RegisterScreen(props) {
   }
 
   return (
-    <div className='form-container'>
-      <Form onSubmit={onSubmit} noValidate className={loading ? 'loading' : ''}>
-        <h1>Register</h1>
-        <Form.Input
-          label='Username'
-          placeholder='Enter username...'
-          type='text'
-          name='username'
-          value={values.username}
-          onChange={onChange}
-          error={errors.username ? true : false}
-        />
-        <Form.Input
-          label='Email'
-          placeholder='Enter email...'
-          type='email'
-          name='email'
-          value={values.email}
-          onChange={onChange}
-          error={errors.email ? true : false}
-        />
-        <Form.Input
-          label='Password'
-          placeholder='Enter password...'
-          type='password'
-          name='password'
-          value={values.password}
-          onChange={onChange}
-          error={errors.password ? true : false}
-        />
-        <Form.Input
-          label='Confirm Password'
-          placeholder='Confirm password...'
-          type='password'
-          name='confirmPassword'
-          value={values.confirmPassword}
-          onChange={onChange}
-          error={errors.confirmPassword ? true : false}
-        />
-        <Button type='submit' primary>
-          Register
-        </Button>
-      </Form>
-      {Object.keys(errors).length > 0 && (
-        <div className='ui error message'>
-          <ul className='list'>
-            {Object.values(errors).map((value) => (
-              <li key={value}>{value}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </div>
+    <Container>
+      <div className='form-container'>
+        <Form
+          onSubmit={onSubmit}
+          noValidate
+          className={loading ? 'loading' : ''}
+        >
+          <h1>Register</h1>
+          <Form.Input
+            label='Username'
+            placeholder='Enter username...'
+            type='text'
+            name='username'
+            value={values.username}
+            onChange={onChange}
+            error={errors.username ? true : false}
+          />
+          <Form.Input
+            label='Email'
+            placeholder='Enter email...'
+            type='email'
+            name='email'
+            value={values.email}
+            onChange={onChange}
+            error={errors.email ? true : false}
+          />
+          <Form.Input
+            label='Password'
+            placeholder='Enter password...'
+            type='password'
+            name='password'
+            value={values.password}
+            onChange={onChange}
+            error={errors.password ? true : false}
+          />
+          <Form.Input
+            label='Confirm Password'
+            placeholder='Confirm password...'
+            type='password'
+            name='confirmPassword'
+            value={values.confirmPassword}
+            onChange={onChange}
+            error={errors.confirmPassword ? true : false}
+          />
+          <Button type='submit' primary>
+            Register
+          </Button>
+        </Form>
+        {Object.keys(errors).length > 0 && (
+          <div className='ui error message'>
+            <ul className='list'>
+              {Object.values(errors).map((value) => (
+                <li key={value}>{value}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </Container>
   )
 }
 

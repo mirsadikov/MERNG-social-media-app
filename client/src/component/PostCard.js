c4a882d9403b487b4c108d838d7f7f912e701728
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-01 19:48:12 +0500

    minor fixes

--- client/src/component/PostCard.js
+++ client/src/component/PostCard.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Card, Icon, Label, Image, Button, Popup } from 'semantic-ui-react'
+import { Card, Icon, Label, Image, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 

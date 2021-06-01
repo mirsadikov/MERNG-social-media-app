58e46d6f46b433a903b831057e2f04bd6d04ba21
Mirabror Sodikov <mirabror9545@gmail.com>
2021-06-01 19:35:28 +0500

    client proxy

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://merng-social-media-ms.herokuapp.com/',
 })
 
 const authLink = setContext(() => {
